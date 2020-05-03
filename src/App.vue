1a8fada4614137fe7245291120b96e71fd9cfcbf
ariel <arielchiaramonte@hotmail.com>
2020-05-03 15:52:48 -0300

    Probando el git!!

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    hola Arielcito!!!
+    hola JP!!!!!
     <router-view/>
   </div>
 </template>
